ff678b4c01390181fd39ce152ed37275d9bafeab
Rhys Tague <54823859+drtaguewsu@users.noreply.github.com>
2024-08-27 20:39:38 +1000

    Remove extra Parenthesis homeRouter
    /Users/sententia/Dropbox/playground/employability-profile/.venv

--- routes/home.js
+++ routes/home.js
@@ -4,7 +4,7 @@ import * as home from '../controllers/home.js';
 export const homeRouter = express.Router()
 
 // Below are all the routes for the home
-homeRouter.get('/', (home.index));
+homeRouter.get('/', home.index);
 
 
 
