ba24c294892de00c57ded1d2534b74b7caac02b3
Rhys Tague <54823859+drtaguewsu@users.noreply.github.com>
2024-08-28 12:33:45 +0000

    Add sample inserts for user table

---
+++ migrations/20240820_3_insert_users.sql
@@ -0,0 +1,11 @@
+INSERT INTO users (username, email, password_hash) VALUES
+('AlexMason', 'alex.mason@example.com', '4f5d4sd4f56sd4f6s4df'),
+('BrookeReid', 'brooke.reid@example.com', '9f8d7s6d575d6f7a88d9'),
+('CharlieG', 'charlie.gonzales@example.net', '54s65f46as5df4as6f5a'),
+('DanaSky', 'dana.sky@example.org', '984c78b34b56c78b34b6'),
+('EvanRush', 'evan.rush@example.com', 'b789c78b97b8c77b89c7'),
+('Fiona_YZ', 'fiona.yz@example.com', '2a5d46ad2a3d46a2d34a'),
+('GabeT1990', 'gabe.t1990@example.net', '98bc4a57bc48a57b48a5'),
+('HollyWood', 'holly.wood@example.org', '4a57b48c56a74c56a74c'),
+('IvyK', 'ivy.k@example.com', 'b34c56b78c56b78c56b7'),
+('JaySmith88', 'jay.smith88@example.net', '78c56b78c56b78c56b78');
