cca8f1121739994f771f531abe38feca02a9cb10
Rhys Tague <54823859+drtaguewsu@users.noreply.github.com>
2024-09-14 05:34:31 +0000

    Add generic id for Product Service stub

--- middleware/productService.js
+++ middleware/productService.js
@@ -2,9 +2,9 @@
 export async function getProduct(id) {
     // Logic to get the product details from the DB
     const products = [
-        {id: 1, name: 'Product', price: 10},
-        {id: 2, name: 'Product', price: 20},
-        {id: 3, name: 'Product', price: 30}
+        {id: id, name: 'Product', price: 10},
+        {id: id, name: 'Product', price: 20},
+        {id: id, name: 'Product', price: 30}
     ];
     return products.find(product => product.id === id);
 }
